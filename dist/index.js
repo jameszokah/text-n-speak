97820a7ad040aba63f92183dfc0d634e18cb057e
jameszokah <jameszokah@gmail.com>
2020-07-05 22:19:21 +0000

    added wave to speaking

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ const pitch = document.getElementById('pitch');
 const form = document.querySelector('form');
 let rateValue = document.querySelector('#rate__value');
 let pitchValue = document.querySelector('#pitch__value');
+const wave = document.getElementById('wave');
 
 let voices = [];
 
@@ -36,16 +37,22 @@ synth.onvoiceschanged = voiceList;
 }
 
 const speak = () => {
+    
+    
+    
     if(synth.speaking) {
+        
         alert('Already speaking ...');
         return;
     }
     if(textInput.value !== '') {
-        
+        wave.style.display = 'block';
 const speakText = new SpeechSynthesisUtterance(textInput.value);
 
+
 speakText.onend = e => {
-    alert('finished speaking');
+    wave.style.display = 'none';
+    
 }
 
 speakText.onerror = e => {
